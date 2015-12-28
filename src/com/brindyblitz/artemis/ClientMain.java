b85133dd5c8036bdced97cdb07b86c081787e047
Jacob Frank <jacobmaxfrank@gmail.com>
2015-12-27 20:51:02 -0800

    Default to fake proxy debug mode, parse CLI args

--- src/com/brindyblitz/artemis/ClientMain.java
+++ src/com/brindyblitz/artemis/ClientMain.java
@@ -1,29 +1,61 @@
 package com.brindyblitz.artemis;
 
-import java.io.IOException;
-
-import com.brindyblitz.artemis.engconsole.EngineeringConsoleManager;
-import com.brindyblitz.artemis.engconsole.RealEngineeringConsoleManager;
 import com.brindyblitz.artemis.engconsole.ui.UserInterfaceFrame;
+import com.brindyblitz.artemis.engconsole.*;
 import com.brindyblitz.artemis.protocol.WorldAwareRobustProxyListener;
+
+import java.io.IOException;
 
 public class ClientMain {
     public static void main(String[] args) throws IOException {
+        String host = "localhost";
+        int port = 2010;
 
-        String host = "192.168.1.116"; //args[0];
-        int port = args.length > 1 ? Integer.parseInt(args[1]) : 2010;
-     
-        new ClientMain(host, port);
+        if (args.length > 2) {
+            System.err.println("Incorrect number of arguments (" + args.length + ")");
+            printUsage();
+            return;
+        }
+
+        if (args.length == 0) {
+            new ClientMain();
+        } else {
+
+            if (args.length >= 1) {
+                host = args[0];
+            }
+            if (args.length == 2) {
+                try {
+                    port = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    System.err.println("Unable to parse port: '" + args[1] + "'!");
+                    printUsage();
+                    return;
+                }
+            }
+
+            new ClientMain(host, port);
+        }
     }
 
+    public ClientMain() {
+        EngineeringConsoleManager engineeringConsoleManager = new FakeEngineeringConsoleManager();
+        buildUIFrame(engineeringConsoleManager);
+    }
 
     public ClientMain(String host, int port) throws IOException {
-    	/* Comment next two lines and uncomment the third line to test the UI without an actual server */
-    	
-    	WorldAwareRobustProxyListener worldAwareRobustProxyListener = new WorldAwareRobustProxyListener(host, port, port);
-    	EngineeringConsoleManager engineeringConsoleManager = new RealEngineeringConsoleManager(worldAwareRobustProxyListener);
-//    	EngineeringConsoleManager engineeringConsoleManager = new FakeEngineeringConsoleManager();
-        
+        WorldAwareRobustProxyListener worldAwareRobustProxyListener = new WorldAwareRobustProxyListener(host, port, port);
+        EngineeringConsoleManager engineeringConsoleManager = new RealEngineeringConsoleManager(worldAwareRobustProxyListener);
+        buildUIFrame(engineeringConsoleManager);
+    }
+
+    private static void buildUIFrame(EngineeringConsoleManager engineeringConsoleManager) {
         new UserInterfaceFrame(engineeringConsoleManager).setVisible(true);
+    }
+
+    private static void printUsage() {
+        System.out.println("Either:\n" +
+                "Zero arguments: the client is set to local debug mode and where no network activity occurs\n" +
+                "Two arguments: <host> <port> (where host is either a hostname, e.g. 'localhost', or an IP address, e.g. '192.168.1.5')");
     }
 }
